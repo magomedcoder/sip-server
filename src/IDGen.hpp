5de2edd7bb9021ec0a46b4650a691370b32e6fc0
Magomedcoder <info@magomedcoder.ru>
2023-02-07 13:13:03 +0300

    Added id generation

---
+++ src/IDGen.hpp
@@ -0,0 +1,31 @@
+//
+// Created by magomedcoder on 07.02.2023.
+//
+
+#ifndef IDGEN_HPP
+#define IDGEN_HPP
+
+#include <iostream>
+#include <sstream>
+
+class IDGen {
+public:
+    IDGen() = delete;
+
+    static std::string GenerateID(int len) {
+        std::ostringstream id;
+        for (int i = 0; i < len; ++i) {
+            id << alphanum[rand() % (sizeof(alphanum) - 1)];
+        }
+
+        return id.str();
+    }
+
+private:
+    static constexpr char alphanum[] =
+            "0123456789"
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz";
+};
+
+#endif
