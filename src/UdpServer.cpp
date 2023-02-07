2b29eb279f51811a640f1ea9d1717848425f3dae
Magomedcoder <info@magomedcoder.ru>
2023-02-07 12:10:10 +0300

    Added udp server

---
+++ src/UdpServer.cpp
@@ -0,0 +1,72 @@
+//
+// Created by magomedcoder on 07.02.2023.
+//
+
+#include "UdpServer.hpp"
+#include <sstream>
+#include <utility>
+#include <vector>
+#include <thread>
+#include <cstring>
+
+UdpServer::UdpServer(std::string ip, int port, OnNewMessageEvent event) : _ip(std::move(ip)),
+                                                                          _port(port),
+                                                                          _onNewMessageEvent(std::move(event)),
+                                                                          _keepRunning(false) {
+    if ((_sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        std::cerr << "не удалось создать сокет" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::memset(&_servaddr, 0, sizeof(_servaddr));
+    _servaddr.sin_family = AF_INET;
+    _servaddr.sin_addr.s_addr = inet_addr(_ip.c_str());
+    _servaddr.sin_port = htons(port);
+    if (bind(_sockfd, reinterpret_cast<const struct sockaddr *>(&_servaddr), sizeof(_servaddr)) < 0) {
+        std::cerr << "сбой привязки" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+UdpServer::~UdpServer() {
+    closeServer();
+}
+
+void UdpServer::startReceive() {
+    _keepRunning = true;
+    _receiverThread = std::thread([=]() {
+        char buffer[BUFFER_SIZE];
+        sockaddr_in senderEndPoint{};
+        std::memset(&senderEndPoint, 0, sizeof(senderEndPoint));
+        int len = sizeof(senderEndPoint);
+        while (_keepRunning) {
+            recvfrom(
+                    _sockfd,
+                    buffer,
+                    BUFFER_SIZE,
+                    0,
+                    reinterpret_cast<struct sockaddr *>(&senderEndPoint),
+                    (socklen_t *) &len
+            );
+            if (!_keepRunning) return;
+            _onNewMessageEvent(buffer, senderEndPoint);
+        }
+    });
+}
+
+int UdpServer::send(sockaddr_in address, const std::string &buffer) const {
+    return sendto(
+            _sockfd,
+            buffer.c_str(),
+            std::strlen(buffer.c_str()),
+            0,
+            reinterpret_cast<const struct sockaddr *>(&address),
+            sizeof(address)
+    );
+}
+
+void UdpServer::closeServer() {
+    _keepRunning = false;
+    shutdown(_sockfd, 2);
+    close(_sockfd);
+    _receiverThread.join();
+}
---
+++ src/UdpServer.hpp
@@ -0,0 +1,43 @@
+//
+// Created by magomedcoder on 07.02.2023.
+//
+
+#ifndef UDP_SERVER_HPP
+#define UDP_SERVER_HPP
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <atomic>
+#include <functional>
+#include <thread>
+
+class UdpServer {
+public:
+    using OnNewMessageEvent = std::function<void(std::string, sockaddr_in)>;
+    static constexpr int BUFFER_SIZE = 2048;
+
+    UdpServer(std::string ip, int port, OnNewMessageEvent event);
+
+    ~UdpServer();
+
+    void startReceive();
+
+    [[nodiscard]] int send(struct sockaddr_in address, const std::string &buffer) const;
+
+private:
+    void closeServer();
+
+    std::string _ip;
+    int _port;
+    int _sockfd;
+    sockaddr_in _servaddr{};
+    OnNewMessageEvent _onNewMessageEvent;
+    std::atomic<bool> _keepRunning;
+    std::thread _receiverThread;
+};
+
+#endif
