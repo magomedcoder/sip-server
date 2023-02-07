ed98963e1be2568429d3f6dad4a6202545bffacd
magomedcoder <info@magomedcoder.ru>
2023-02-07 20:19:10 +0300

    Added README.md and LICENSE

---
+++ README.md
@@ -0,0 +1,15 @@
+## SIP-сервер для обработки VoIP-вызовов.
+
+# Сборка
+
+```bash
+  mkdir build && cd build
+  cmake ..
+  make
+```
+
+# Запуск
+
+```bash
+./sip-server --ip=IP адрес --port=5060
+```
--- main.cpp
+++ main.cpp
@@ -3,19 +3,15 @@
 #include "Lib/cxxopts.hpp"
 
 int main(int argc, char **argv) {
-    cxxopts::Options options("Sip-server", "Сервер для обработки вызовов");
-
+    cxxopts::Options options("Sip-server", "SIP-сервер для обработки VoIP-вызовов.");
     options.add_options()
             ("ip", "IP адрес", cxxopts::value<std::string>())
             ("port", "Порт", cxxopts::value<int>()->default_value(std::to_string(5060)));
-
     auto result = options.parse(argc, argv);
-
     if (result.count("р")) {
         std::cout << options.help() << std::endl;
         exit(0);
     }
-
     try {
         std::string ip = result["ip"].as<std::string>();
         int port = result["port"].as<int>();
