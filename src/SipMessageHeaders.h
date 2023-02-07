3d52b7af8aa43ae4a8685bb55d54c3709cf4204a
Magomedcoder <info@magomedcoder.ru>
2023-02-07 14:23:20 +0300

    Added message headers

---
+++ src/SipMessageHeaders.h
@@ -0,0 +1,22 @@
+//
+// Created by magomedcoder on 07.02.2023.
+//
+
+#ifndef SIP_MESSAGE_HEADERS_H
+#define SIP_MESSAGE_HEADERS_H
+
+class SipMessageHeaders {
+public:
+    SipMessageHeaders() = delete;
+
+    static constexpr auto VIA = "Via";
+    static constexpr auto FROM = "From";
+    static constexpr auto TO = "To";
+    static constexpr auto CALL_ID = "Call-ID";
+    static constexpr auto CSEQ = "CSeq";
+    static constexpr auto CONTACT = "Contact";
+    static constexpr auto CONTENT_LENGTH = "Content-Length";
+    static constexpr auto HEADERS_DELIMETER = "\r\n";
+};
+
+#endif
