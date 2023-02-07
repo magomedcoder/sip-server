f1cd8119ee78253a682ad09d7e6c056b11858de8
Magomedcoder <info@magomedcoder.ru>
2023-02-07 13:52:45 +0300

    Added command options parser

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include "SipServer.hpp"
+#include "Lib/cxxopts.hpp"
 
-int main() {
-    SipServer server("127.0.0.1", 5060);
-    std::cout << "Сервер запущен" << std::endl;
+int main(int argc, char **argv) {
+    cxxopts::Options options("Sip-server", "Сервер для обработки вызовов");
+
+    options.add_options()
+            ("ip", "IP адрес", cxxopts::value<std::string>())
+            ("port", "Порт", cxxopts::value<int>()->default_value(std::to_string(5060)));
+
+    auto result = options.parse(argc, argv);
+
+    if (result.count("р")) {
+        std::cout << options.help() << std::endl;
+        exit(0);
+    }
+
+    try {
+        std::string ip = result["ip"].as<std::string>();
+        int port = result["port"].as<int>();
+        SipServer server(ip, port);
+        std::cout << "Сервер запущен." << std::endl;
+        getchar();
+    } catch (const cxxopts::OptionException &) {
+        std::cout << "Пожалуйста, введите ip и порт." << std::endl;
+    }
     return 0;
 }
